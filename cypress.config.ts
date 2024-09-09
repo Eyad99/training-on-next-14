671a318ca1dbb9c0c760301c9f7eb6a7b281ddff
eyad sharaf almasri <iead.sh123@gmail.com>
2024-09-09 16:58:31 +0300

    instal cypress

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+});
+
+// experimentalStudio: true,
+// viewportHeight: 1000,
+// viewportWidth: 450,
