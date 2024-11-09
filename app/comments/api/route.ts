4884fc8f4c842e3cc44c35f365a0e09f50dc2316
eyad sharaf almasri <iead.sh123@gmail.com>
2024-11-10 02:03:58 +0300

    api methods

--- app/comments/api/route.ts
+++ app/comments/api/route.ts
@@ -1,26 +1,64 @@
-import { NextRequest } from "next/server";
-import { Comments, Connent_Req } from "../data";
+import { NextRequest, NextResponse } from 'next/server';
+import { Comments, Connent_Req } from '../data';
 
-export async function GET(req: NextRequest) {
-  const url = req.nextUrl.searchParams;
-  const query = url.get("query");
-  return Response.json(Comments);
+// export async function GET(req: NextRequest) {
+//   const url = req.nextUrl.searchParams;
+//   const query = url.get("query");
+//   return Response.json(Comments);
+// }
+
+export async function GET() {
+	return Response.json(Comments);
 }
 
 export async function POST(request: Request) {
-  const comment = await request.json();
-  console.log("requestrequestrequestrequestrequest", comment);
-  const newComment = {
-    id: Comments.length + 1,
-    name: comment.name,
-    address: comment.address,
-  };
+	const comment = await request.json();
+	const newComment = {
+		id: Comments.length + 1,
+		name: comment.name,
+		address: comment.address,
+	};
 
-  console.log("12323213", JSON.stringify(newComment));
-  console.log("12323213 newCommentnewComment", newComment);
-  Comments.push(newComment);
-  return new Response(JSON.stringify(newComment), {
-    headers: { "Content-Type": "application/json" },
-    status: 201,
-  });
+	Comments.push(newComment);
+	return new Response(JSON.stringify(newComment), {
+		headers: { 'Content-Type': 'application/json' },
+		status: 201,
+	});
+}
+
+export async function PUT(request: NextRequest) {
+	try {
+		const formData = await request.formData();
+		const selectedId = formData.get('id') as string;
+		const selectedName = formData.get('name') as string;
+
+		const commentInd = Comments.findIndex((item: any) => item.id == selectedId);
+		if (commentInd !== -1) {
+			Comments[commentInd].name = selectedName;
+		} else {
+			return NextResponse.json({ message: 'id not found' }, { status: 400 });
+		}
+		return NextResponse.json({ message: 'Updated Successfuly' }, { status: 200 });
+	} catch (error) {
+		console.log('error', error);
+		return NextResponse.json({ message: 'error' }, { status: 500 });
+	}
+}
+
+export async function DELETE(request: NextRequest) {
+	try {
+		const url = request.nextUrl.searchParams;
+		const id = url.get('id');
+
+		const commentInd = Comments.findIndex((item: any) => item.id == id);
+		if (commentInd !== -1) {
+			Comments.splice(commentInd, 1);
+		} else {
+			return NextResponse.json({ message: 'id not found' }, { status: 400 });
+		}
+		return NextResponse.json({ message: 'Deleted Successfuly' }, { status: 200 });
+	} catch (error) {
+		console.log('error', error);
+		return NextResponse.json({ message: 'error' }, { status: 500 });
+	}
 }
