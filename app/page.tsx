875a35701bef1eced344024051aceafdd9b051b8
eyad sharaf almasri <iead.sh123@gmail.com>
2024-12-07 23:42:20 +0300

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export default async function Home() {
-	console.log('log');
-	return (
+ 	return (
 		<main className='flex flex-col justify-center items-center'>
 			<h1 className='text-2xl text-center font-bold'>Home Page</h1>
 		</main>
