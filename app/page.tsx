84cf60f1406a3deec64ed43a80dc973d6efe242a
eyad sharaf almasri <iead.sh123@gmail.com>
2024-12-14 23:50:46 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default async function Home() {
- 	return (
+	conosle.log('start again');
+	return (
 		<main className='flex flex-col justify-center items-center'>
 			<h1 className='text-2xl text-center font-bold'>Home Page</h1>
 		</main>
