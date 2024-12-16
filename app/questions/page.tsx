7c1629e5422f76cc62194624ca854c741befeeac
eyad sharaf almasri <iead.sh123@gmail.com>
2024-12-16 15:40:01 +0300

    Some improvements

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,16 +1,16 @@
+import React from 'react';
 import QuestionsAnswers from '@/components/CComponents/QuestionsList/Answers/page';
 import QuestionsList from '@/components/CComponents/QuestionsList/page';
 import { Tabs } from '@radix-ui/react-tabs';
-import React from 'react';
 
 const Questions = () => {
 	return (
 		<Tabs className='flex' defaultValue='1'>
-			<aside className='fixed hidden max-h-[calc(100vh-4rem)] w-[20%] shrink-0 border-r md:block overflow-auto'>
+			<aside className='fixed hidden max-h-[calc(100vh-4rem)] w-[20%] shrink-0 border-r md:block overflow-auto p-2'>
 				<QuestionsList />
 			</aside>
 
-			<section className=' md:w-[80%] md:ml-[20%] w-full px-2'>
+			<section className='md:w-[80%] md:ml-[20%] w-full px-2'>
 				<QuestionsAnswers />
 			</section>
 		</Tabs>
