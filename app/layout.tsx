27f7206d6221e5b2482eedbcea8c079b6be94349
eyad sharaf almasri <iead.sh123@gmail.com>
2025-03-15 03:14:57 +0300

    add container

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,12 +28,18 @@ export default function RootLayout({
 				<head>
 					<link rel='icon' href='/next.svg' sizes='any' />
 				</head>
-				<body className={inter.className}>
+				<body className={inter.className + ' bg-gray-50'}>
 					<ReactQueryProvider>
 						<HydrationBoundary state={dehydrate(queryClient)}>
 							<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
 								<Header />
-								<section>{children}</section>
+
+								<section className='container mx-auto my-4'>
+									<div className='grid auto-rows-min gap-4 grid-cols-12'>
+										<div className='rounded-md md:col-span-4 col-span-12 bg-fuchsia-300'>Info</div>
+										<div className='rounded-md md:col-span-8 col-span-12 bg-slate-300'>{children}</div>
+									</div>
+								</section>
 							</ThemeProvider>
 						</HydrationBoundary>
 					</ReactQueryProvider>
